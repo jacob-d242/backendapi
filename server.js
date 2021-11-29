f2c63f73979e03cdf6e69117b5e180797a70d186
jacob-d242 <jacobmoracha@gmail.com>
2021-11-29 13:10:08 +0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
@@ -11,7 +12,10 @@ import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
